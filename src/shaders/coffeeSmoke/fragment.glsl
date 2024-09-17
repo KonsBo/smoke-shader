45b93c02ec3799fdfadb700a2a2ec44dfdb1a632
Konstantina Boua <85695279+KonsBo@users.noreply.github.com>
2024-09-17 12:57:54 +0300

    Added Geometry, Material, Mesh, Shaders, Load the perlin noise

--- src/shaders/coffeeSmoke/fragment.glsl
+++ src/shaders/coffeeSmoke/fragment.glsl
@@ -1,7 +1,9 @@
+varying vec2 vUv;
+
 void main()
 {
     //Final color
-    gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+    gl_FragColor = vec4(vuv, 0.0, 1.0);
     #include <tonemapping_fragment>
      #include <colorspace_fragment>
 }
--- src/shaders/coffeeSmoke/vertex.glsl
+++ src/shaders/coffeeSmoke/vertex.glsl
@@ -1,5 +1,10 @@
+varying vec2 vuv;
+
 void main()
 {
     //Final position
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+
+    //Varying
+    vUv = uv;
 }
