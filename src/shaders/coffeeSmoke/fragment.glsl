49a75b51b40d63bc51c9c0525b9f60ea2a4d6dad
Konstantina Boua <85695279+KonsBo@users.noreply.github.com>
2024-09-17 14:39:59 +0300

    fade edges smooth

--- src/shaders/coffeeSmoke/fragment.glsl
+++ src/shaders/coffeeSmoke/fragment.glsl
@@ -17,6 +17,15 @@ void main()
 
      // remap
      smoke = smoothstep(0.4, 1.0, smoke);
+     
+
+     // edges smooth
+     
+    smoke *= smoothstep(0.0, 0.1, vUv.x);
+    smoke *= smoothstep(1.0, 0.9, vUv.x);
+    smoke *= smoothstep(0.0, 0.1, vUv.y);
+    smoke *= smoothstep(1.0, 0.4, vUv.y);
+
 
     //Final color
     gl_FragColor = vec4(1.0,1.0,1.0,smoke);
